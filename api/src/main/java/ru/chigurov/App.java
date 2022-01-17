2a720cf83428cfe2416e43906948b615655c14ea
MikHail854 <vip.moneta95@mail.ru>
2022-01-18 01:18:43 +0700

    added custom collection arrayList

--- api/src/main/java/ru/chigurov/App.java
+++ api/src/main/java/ru/chigurov/App.java
@@ -5,6 +5,10 @@ public class App {
     public static void main(String[] args) {
         Utils utils = new Utils();
         System.out.println(utils.isAllPositiveNumbers("12","79"));
+
+        ArrayListCustom<String> arrayListCustom = new ArrayListCustom<String>();
+        arrayListCustom.add("Custom collection is working");
+        System.out.println(arrayListCustom);
     }
 
 }
